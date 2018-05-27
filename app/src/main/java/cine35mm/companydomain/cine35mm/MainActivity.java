12641cdcc19a764d4a5b750856f8ead68bcfa729
jeesquivel <esquivelsamudio95@gmail.com>
2018-05-27 10:42:04 -0600

    Initial commit

---
+++ app/src/main/java/cine35mm/companydomain/cine35mm/MainActivity.java
@@ -0,0 +1,13 @@
+package cine35mm.companydomain.cine35mm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
