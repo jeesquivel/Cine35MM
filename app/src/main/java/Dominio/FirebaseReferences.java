48fe0d2dc1c616ef5c1d262bde2d84e4b1365620
jeesquivel <esquivelsamudio95@gmail.com>
2018-05-27 12:29:07 -0600

    INICIO DE SESION

---
+++ app/src/main/java/Dominio/FirebaseReferences.java
@@ -0,0 +1,11 @@
+package Dominio;
+
+/**
+ * Created by Jeison on 27/05/2018.
+ */
+
+public class FirebaseReferences {
+    final public static String TUTORIALES_REFERENCE = "tutorial";
+    final public static String USUARIOS_REFERENCE = "Usuarios";
+
+}
